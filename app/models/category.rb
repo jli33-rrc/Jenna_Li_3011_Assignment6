0d2862b73e5ee61b04c97015d439f4af8a8f7373
Jenna <jli33@academic.rrc.ca>
2025-06-28 11:31:48 -0500

    Add associations to the Product and Category models. [Part 2, step 4]

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :products, dependent: :nullify # Nullifies category_id if category is deleted
+    validates :name, presence: true, uniqueness: true
 end
